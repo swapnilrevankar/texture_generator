9c74bc9df5c113e6fc8cee56748ec068d7f6d796
Swapnil Revankar <swapnil_revankar2004@yahoo.com>
2022-03-11 18:15:20 +0200

    Banuba package

---
+++ TextureGenerator01/Assets/BanubaFaceAR/BaseAssets/Scripts/FacesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace BNB
+{
+    public class FacesController : MonoBehaviour
+    {
+        GameObject[] faces;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            faces = new GameObject[1];
+            faces[0] = GameObject.Find("Face0");
+
+            BanubaSDKManager.instance.onRecognitionResult += onRecognitionResult;
+        }
+
+        void onRecognitionResult(FrameData frameData)
+        {
+            var error = IntPtr.Zero;
+            var res = BanubaSDKBridge.bnb_frame_data_has_frx_result(frameData, out error);
+            Utils.CheckError(error);
+
+            if (!res)
+                return;
+
+            var face_count = BanubaSDKBridge.bnb_frame_data_get_face_count(frameData, out error);
+            Utils.CheckError(error);
+            InstantiateFaces(face_count);
+        }
+
+        void InstantiateFaces(int face_count)
+        {
+            var len = faces.Length;
+            if (len < face_count) {
+                Array.Resize(ref faces, face_count);
+                for (int i = len; i < face_count; ++i) {
+                    faces[i] = GameObject.Instantiate(faces[0], transform);
+                    faces [i]
+                        .SetActive(true);
+                    faces[i].name = "Face" + i;
+
+                    var faceController = faces[i].GetComponent<FaceController>();
+                    faceController.faceIndex = i;
+                }
+            }
+        }
+    }
+
+}
